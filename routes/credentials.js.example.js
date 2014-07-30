06a72177a5ab29bda5f84b9381e2e0383f418add
John J Czaplewski <jczaplew@gmail.com>
2014-07-30 10:36:24 -0500

    Update example credentials to include pg variables and bump version

--- routes/credentials.js.example.js
+++ routes/credentials.js.example.js
@@ -1,8 +1,15 @@
-exports.credentials = {
+exports.mysql = {
   host     : 'localhost',
   user     : 'user',
   password : 'password',
   database : 'playground',
   socketPath: '/tmp/mysql.sock'
 //  socketPath: '/var/tmp/mariadb.sock'
-}
+};
+exports.pg = {
+  host     : 'localhost',
+  port     : '5432',
+  user     : 'postgres',
+  password : '',
+  database : 'alice'
+};
