5dcb10f3976b3f6fcb8e5f08be99fc32ecb22b44
John J Czaplewski <jczaplew@gmail.com>
2014-07-31 15:36:57 -0500

    Revert back to node-mysql....

--- larkin.js
+++ larkin.js
@@ -3,8 +3,7 @@ var mysql = require("mysql"),
     async = require("async"),
     winston = require("winston"),
     credentials = require("./routes/credentials"),
-    csv = require('express-csv'),
-    Client = require('mariasql');
+    csv = require('express-csv');
 
 winston.add(winston.transports.File, { filename: "logs/larkin.log" });
 
@@ -12,19 +11,19 @@ winston.add(winston.transports.File, { filename: "logs/larkin.log" });
   var larkin = {};
 
   larkin.connectMySQL = function() {
+    this.connection = mysql.createConnection(credentials.mysql);
 
-    this.connection = new Client();
-    this.connection.connect(credentials.mysql);
+    this.connection.connect(function(error) {
+      if (error) {
+        this.log("error", error);
+      } else {
+        this.log("info", "Connected to MySQL");
+      }
+    }.bind(this));
 
-    this.connection.on('connect', function() {
-      this.log("info", "Connected to MySQL");
-    }.bind(this))
-    .on('error', function(err) {
+    this.connection.on("error", function(err) {
       this.log("error", err);
       this.connectMySQL();
-    }.bind(this))
-    .on('close', function(error) {
-      this.log("error", error);
     }.bind(this));
   };
 
@@ -50,27 +49,21 @@ winston.add(winston.transports.File, { filename: "logs/larkin.log" });
   }
 
   larkin.query = function(sql, params, callback, send, res, format, next) {
-    this.connection.query(sql, params)
-      .on("result", function(result) {
-        result.results = [];
-        result.on("row", function(row) {
-          result.results.push(row);
-        })
-        .on("error", function(err) {
-          callback(err);
-          this.log("error", err);
-        }.bind(this))
-        .on("end", function(info) {
-          if (send) {
-            this.sendData(result.results, res, format, next);
-          } else {
-            callback(null, result.results);
-          }
-        }.bind(this));
-      }.bind(this))
-      .on("end", function() {
-        // don't have to do anything
-      });
+    this.connection.query(sql, params, function(error, result) {
+      if (error) {
+        if (callback) {
+          callback(error);
+        } else {
+          this.error(res, next, "Error retrieving from MySQL.", error);
+        }
+      } else {
+        if (send) {
+          this.sendData(result, res, format, next);
+        } else {
+          callback(null, result);
+        }
+      }
+    }.bind(this));
   };
 
 
