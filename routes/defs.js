6bf234e33f2dda0f1cc945504bc98d93cb626f23
Shanan Peters <cambro@gmail.com>
2014-08-19 22:17:35 -0400

    added some definitions

--- routes/defs.js
+++ routes/defs.js
@@ -5,16 +5,17 @@
 
   // Instead of adding metadata to each route in api.js, we are going to do it here
   defs["/column"] = {
-    "description": "Gets all attributes of a given column",
+    "description": "Get all units of a given column, optionally with geometry",
     "options": {
       "parameters": {
-        "id": "Get a column by id",
-        "lat": "A valid latitude",
-        "lng": "A valid longitude",
+        "id": "integer, get a column by unique identifier",
+        "lat": "nuumber, decimal degree latitude, WGS84",
+        "lng": "number, decimal degree longitude, WGS84",
+        "geom": "boolean, whether associated geometry is returned",
         "response": "Can be 'short' or 'long'"
       },
       "output_formats": "json",
-      "examples": ["api/columns?id=17", "api/columns?lat=50&lng=-80"],
+      "examples": ["?id=17", "?lat=50&lng=-80"],
       "fields": {
         "short": [
           "id",
@@ -29,7 +30,8 @@
           "min_thick",
           "color",
           "lith_type",
-          "pbdb"
+          "pbdb",
+          "geom"
         ],
         "long": [
           "FO_interval",
@@ -41,16 +43,72 @@
           "LO_age",
           "t_age",
           "position_bottom",
-          "notes"
+          "notes",
         ]
       }
     }
   };
+  defs["/columns"] = {
+    "description": "Get all colums containing one or more units matching specfied search criteria",
+    "options": {
+      "parameters": {
+        "interval_name": "text, name of search time interval",
+        "age": "number, search age in Myr before present",
+        "age_top": "number, youngest limit of search, in Myr before present - must be used with age_bottom",
+        "age_bottom": "number, oldest limit of search, in Myr before present -  must be used with age_top",
+        "format": "string, Desired output format"
+      },
+      "examples": ["?interval_name=Permian", "?age=271", "?age_top=200&age_bottom=250"],
+      "fields": {
+        "geometry",
+        "col_id",
+
+
+      }
+    }
+
+
+  };
 
   // This is the primary dictionary for all field definitions
   defs.define = {
-    "id": "An id",
-    "strat_name": "Strat name"
+    "id": "integer, unique identifier",
+    "unit_id": "integer, unique identifier for unit",
+    "section_id": "integer, unique identifier for section (package)",
+    "col_id": "integer, unique identifier for column",
+    "strat_name": "text, informal unit name",
+    "Mbr": "text, lithostratigraphic member",
+    "Fm": "text, lithostratigraphic formation",
+    "Gp": "text, lithostratigraphic group",
+    "SGp": "text, lithostratigraphic supergroup",
+    "era": "string, containing international chronostratigraphic period",
+    "period": "string, containing international chronostratigraphic period",
+    "max_thick": "number, maximum thickness in meters",
+    "min_thick": "number, minimum thickess in meters (NB some zero values may not have meaning)",
+    "color": "text, recommended coloring for units baesd on dominant lithology",
+    "FO_interval": "text, chronostratigraphic interval containing initiation/earliest(oldest) age",
+    "FO_h": "integer, incates position within interval of initiation, 0=at base of interval",
+    "FO_age": "number, age of FO_interval base in Myr before present",
+    "b_age": "number, continuous time age model estimated for initiation, in Myr before present",
+    "early_age": "number, oldest age, Myr before present",
+    "LO_interval": "text, chronostratigraphic interval containing truncation/latest(youngest) age",
+    "LO_h": "integer, incates position within interval of truncation, 0=at top of interval",
+    "LO_age": "number, age of FO_interval top in Myr before present",
+    "t_age": "number, continuous time age model estimated for truncation, in Myr before present",
+    "late_age": "number, youngest age, Myr before present",
+    "position_bottom": "number, estimated position of unit relative to other units in section",
+    "lith": "text, specific lithology",
+    "lith_type": "text, general lithology type",
+    "lith_class": "text, general lithology class",
+    "environ": "text, specific environment",
+    "environ_type": "general environment type",
+    "environ_class": "text, general lithology class",
+    "pbdb": "number of matching Paleobiology Database fossil collections",
+    "units": "integer, number of units",
+    "notes": "text, notes releavnt to containing element",
+    "project": "text, name of project",
+    "geom": "geometry",
+    "area": "area in square kilometers"
   };
 
   // Given a route and field type(optional), will return all field definitions
