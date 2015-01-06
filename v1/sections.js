14cc6877548e354ac2e6ec1665e150ff6098892a
John J Czaplewski <jczaplew@gmail.com>
2015-01-06 13:08:12 -0600

    Add top and bottom age to /sections and update metadata

--- v1/sections.js
+++ v1/sections.js
@@ -16,7 +16,7 @@ module.exports = function(req, res, next) {
   } else {
     return larkin.error(req, res, next, "An invalid parameter was passed");
   }
-  larkin.query("SELECT s.id, s.col_id, ints.interval_name AS bottom, ints2.interval_name AS top, count(u.id) AS units, COALESCE(r.fossils,0) AS fossils \
+  larkin.query("SELECT s.id, s.col_id, ints2.interval_name AS top, min(t1_age) AS top_age, ints.interval_name AS bottom, max(t1_age) AS bottom_age, count(u.id) AS units, COALESCE(r.fossils,0) AS fossils \
    FROM sections s \
     JOIN intervals ints on FO = ints.id \
     JOIN intervals ints2 on LO = ints2.id \
@@ -26,6 +26,7 @@ module.exports = function(req, res, next) {
         JOIN pbdb_matches pbdb on pbdb.unit_id = u.id \
         GROUP BY u.section_id \
     ) r ON r.section_id = s.id \
+    JOIN unit_boundaries ub ON s.id = ub.section_id \
    " + where +    
   " GROUP BY s.id", params, function(error, result) {
     if (error) {
