1250acd4ed2fc40f0a89e61dc19cec8444f4b423
John J Czaplewski <jczaplew@gmail.com>
2015-01-21 12:34:11 -0600

    Added strat_name and strat_id parameters to /units

--- v1/units.js
+++ v1/units.js
@@ -21,6 +21,23 @@ module.exports = function(req, res, next) {
             }
           }
         });
+      } else if (req.query.strat_name) {
+        larkin.query("SELECT strat_name_id FROM lookup_strat_names WHERE strat_name LIKE ? ", ["%" + req.query.strat_name + "%"], function(error, result) {
+          if (error) {
+            callback(error);
+          } else {
+            if (result.length === 0) {
+              larkin.error(req, res, next, "No results found");
+            } else {
+              var ids = result.map(function(d) { return d.strat_name_id });
+              callback(null, {"interval_name": "none", "age_bottom": 99999, "age_top": 0, "strat_ids": ids });
+            }
+          }
+        });
+
+      } else if (req.query.strat_id) {
+        var ids = req.query.strat_id.split(",").map(function(d) { return parseInt(d) });
+        callback(null, {"interval_name": "none", "age_bottom": 99999, "age_top": 0, "strat_ids": ids });
       } else if (req.query.age) {
         callback(null, {"interval_name": "none", "age_bottom": req.query.age, "age_top": req.query.age});
       } else if (req.query.age_top && req.query.age_bottom) {
@@ -68,6 +85,11 @@ module.exports = function(req, res, next) {
         params.push(req.query.col_id);
       }
 
+      if (req.query.strat_name || req.query.strat_id) {
+        where += " AND lookup_strat_names.strat_name_id IN (?) OR lookup_strat_names.bed_id IN (?) OR lookup_strat_names.mbr_id IN (?) OR lookup_strat_names.fm_id IN (?)";
+        params.push(data.strat_ids, data.strat_ids, data.strat_ids, data.strat_ids);
+      }
+
       var shortSQL = "units.id AS id,units_sections.section_id as section_id, units_sections.col_id as col_id, col_area, units.strat_name, units.position_bottom, mbr_name Mbr, fm_name Fm, gp_name Gp, sgp_name SGp, era, period, max_thick, min_thick, color AS u_color, lith_type, count(distinct collection_no) pbdb";
             
       var longSQL = "units.id AS id, units_sections.section_id as section_id, project_id, units_sections.col_id as col_id, col_area, units.strat_name, mbr_name Mbr, fm_name Fm, gp_name Gp, sgp_name SGp, era, period, max_thick,min_thick, lith_class, lith_type, lith_long lith, environ_class, environ_type, environ, count(distinct collection_no) pbdb, FO_interval, FO_h, FO_age, LO_interval, LO_h, LO_age, position_bottom, notes, units.color AS u_color, colors.unit_hex AS color, colors.text_hex AS text_color, min(ubt.t1_age) AS t_age, GROUP_CONCAT(distinct ubt.unit_id_2 SEPARATOR '|') AS units_above, max(ubb.t1_age) AS b_age, GROUP_CONCAT(distinct ubb.unit_id SEPARATOR '|') AS units_below "; 
@@ -84,7 +106,7 @@ module.exports = function(req, res, next) {
             " + ((req.query.response === "long") ? "LEFT JOIN unit_notes ON unit_notes.unit_id=units.id JOIN colors ON units.color = colors.color" : "") + " \
             LEFT JOIN pbdb_matches ON pbdb_matches.unit_id=units.id \
             WHERE status_code='active' AND FO_age > ? AND LO_age < ? AND units.id IN (SELECT unit_liths.unit_id from unit_liths JOIN liths on lith_id=liths.id WHERE ?? LIKE ?)" + where + " GROUP BY units.id ORDER BY t_age ASC";
-     
+      
       larkin.query(sql, params, function(error, result) {
           if (error) {
             callback(error);
