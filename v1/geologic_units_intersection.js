c980e7cbf0d97610eb96e79fcdd21de361f9c05b
John J Czaplewski <jczaplew@gmail.com>
2015-02-10 13:49:43 -0600

    Updates to geologic_units routes

--- v1/geologic_units_intersection.js
+++ v1/geologic_units_intersection.js
@@ -7,15 +7,16 @@ module.exports = function(req, res, next) {
   if (Object.keys(req.query).length < 1) {
     return larkin.info(req, res, next);
   }
-  if (!req.query.line || !req.query.type) {
-    return larkin.error(req, res, next, "A valid WKT line and geometry type are required");
+  if (!(req.query.line || req.query.shape) || !req.query.type) {
+    return larkin.error(req, res, next, "A valid WKT shape and map type are required");
   }
 
   var type = (["gmna", "gmus"].indexOf(req.query.type) > -1) ? req.query.type : "gmna",
-      buffer = (req.query.buffer && !isNaN(parseInt(req.query.buffer))) ? parseInt(req.query.buffer)*1000 : 35000
+      buffer = (req.query.buffer && !isNaN(parseInt(req.query.buffer))) ? parseInt(req.query.buffer)*1000 : 35000,
+      shape = (req.query.line) ? req.query.line : req.query.shape;
 
   if (type === "gmna") {
-    larkin.queryPg("earthbase", "WITH linestring AS (SELECT ST_Buffer($1::geography, $2) AS buffer) SELECT objectid AS id, unit_abbre, rocktype, lithology AS lith, min_age, max_age, interval_color AS color, ST_AsGeoJSON(ST_Intersection(the_geom, (SELECT buffer FROM linestring))) AS geometry FROM gmna.gmna_for_maps WHERE ST_Intersects(the_geom, (SELECT buffer FROM linestring)) is true", [req.query.line, buffer], function(error, result) {
+    larkin.queryPg("earthbase", "WITH linestring AS (SELECT ST_Buffer($1::geography, $2) AS buffer) SELECT objectid AS id, unit_abbre, rocktype, lithology AS lith, min_age, max_age, interval_color AS color, ST_AsGeoJSON(ST_Intersection(the_geom, (SELECT buffer FROM linestring))) AS geometry FROM gmna.gmna_for_maps WHERE ST_Intersects(the_geom, (SELECT buffer FROM linestring)) is true", [shape, buffer], function(error, result) {
       if (error) {
         larkin.error(req, res, next, "Something went wrong");
       } else {
@@ -40,22 +41,39 @@ module.exports = function(req, res, next) {
       }
     });
   } else {
-    larkin.queryPg("earthbase", "WITH linestring AS (SELECT ST_Buffer($1::geography, $2) AS buffer), states as (SELECT postal FROM us_states WHERE ST_Intersects(geom::geography, (SELECT buffer FROM linestring))), subset as (SELECT * FROM gmus.geologic_units_with_intervals WHERE upper(state) in (SELECT postal FROM states)) SELECT gid AS id, state, a.rocktype1, a.rocktype2, b.rocktype3, a.unit_name, b.unit_age, a.unitdesc, a.strat_unit, a.unit_com, a.interval_color AS color, ST_AsGeoJSON(ST_Intersection(the_geom, (SELECT buffer FROM linestring))) AS geometry FROM subset a JOIN gmus.units b ON a.unit_link = b.unit_link WHERE ST_Intersects(the_geom::geography, (SELECT buffer FROM linestring)) is true", [req.query.line, buffer], function(error, result) {
-      if (error) {
-        larkin.error(req, res, next, "Something went wrong");
+    larkin.queryPg("earthbase", "SELECT ST_Area(ST_Buffer(ST_GeomFromText($1), $2)) AS area", [shape, buffer], function(err, data) {
+      if (err) {
+        return larkin.error(req, res, next, "Invalid geometry supplied");
+      }
+      if (data.rows[0].area > 25) {
+        return larkin.error(req, res, next, "Area too large. Please select a smaller area.");
       } else {
-        dbgeo.parse({
-          "data": result.rows,
-          "outputFormat": (req.query.format) ? larkin.getOutputFormat(req.query.format) : "geojson",
-          "callback": function(error, result) {
-            if (error) {
-              larkin.error(req, res, next, "Something went wrong");
-            } else {
-              larkin.sendCompact(gp(result, 4), res, "json", next);
-            }
+        larkin.queryPg("earthbase", "WITH linestring AS (SELECT ST_Buffer($1::geography, $2) AS buffer), states as (SELECT postal FROM us_states WHERE ST_Intersects(geom::geography, (SELECT buffer FROM linestring))), subset as (SELECT * FROM gmus.geologic_units_with_intervals WHERE upper(state) in (SELECT postal FROM states)) SELECT gid AS id, state, a.rocktype1, a.rocktype2, b.rocktype3, a.unit_name, b.unit_age, a.unitdesc, a.strat_unit, a.unit_com, a.interval_color AS color, ST_AsGeoJSON(ST_Intersection(the_geom, (SELECT buffer FROM linestring))) AS geometry FROM subset a JOIN gmus.units b ON a.unit_link = b.unit_link WHERE ST_Intersects(the_geom::geography, (SELECT buffer FROM linestring)) is true", [shape, buffer], function(error, result) {
+          if (error) {
+            larkin.error(req, res, next, "Something went wrong");
+          } else {
+            dbgeo.parse({
+              "data": result.rows,
+              "outputFormat": (req.query.format) ? larkin.getOutputFormat(req.query.format) : "geojson",
+              "callback": function(error, result) {
+                if (error) {
+                  larkin.error(req, res, next, "Something went wrong");
+                } else {
+                  if (larkin.getOutputFormat(req.query.format) === "geojson") {
+                    result = gp(result, 4);
+                  }
+                  if (api.acceptedFormats.bare[req.query.format]) {
+                    larkin.sendBare(result, res, next);
+                  } else {
+                    larkin.sendCompact(result, res, null, next);
+                  }
+                }
+              }
+            });
           }
         });
       }
     });
+        
   } 
 }
