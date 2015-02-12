d7acbd7a44be71b447a75da0d49cc44802cb041a
Shanan Peters <cambro@gmail.com>
2015-02-12 12:37:43 -0600

    order of build scripts fixed

--- v1/utilities/rebuild.sh
+++ v1/utilities/rebuild.sh
@@ -4,8 +4,8 @@ source scripts/credentials.py
 mysql -u $mysql_user -p$mysql_passwd $mysql_db < scripts/redo_unit_boundaries.sql
 echo "Done reloading boundaries into database"
 
-python scripts/build_lookup_strat_names.py
 python scripts/build_lookup_unit_intervals.py
+python scripts/build_lookup_strat_names.py
 python scripts/build_lookup_unit_liths.py
 python scripts/build_pbdb_matches.py
 
