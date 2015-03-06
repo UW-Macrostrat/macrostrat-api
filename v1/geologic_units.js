66329b2eb1ce5579d56ace7d57f3157e3eeffd83
John J Czaplewski <jczaplew@gmail.com>
2015-03-06 08:56:26 -0600

    Migrated geologic_units to geomacro

--- v1/geologic_units.js
+++ v1/geologic_units.js
@@ -10,75 +10,44 @@ module.exports = function(req, res, next) {
     larkin.info(req, res, next);
   } else {
     var geo = (req.query.format && api.acceptedFormats.geo[req.query.format]) ? true : false,
-        types = (req.query.type) ? req.query.type.split(",") : ["gmna", "gmus", "column"];
+        types = (req.query.type) ? req.query.type.split(",") : ["gmna", "gmus"];
 
     if (["gmna", "gmus"].indexOf(req.query.type) < 0) {
       return larkin.error(req, res, next, "Please enter a valid map type to query (gmna or gmus)");
     }
 
     if (req.query.type === "gmus") {
-      var where = "",
+      var where = [],
           params = [];
 
       if (req.query.lat && req.query.lng) {
-        var placeholder = "$" + (params.length + 1);
-        where += " WHERE ST_Contains(the_geom, ST_GeomFromText(" + placeholder + ", 4326))";
+        where.push(" ST_Contains(the_geom, ST_GeomFromText($" + (where.length + 1) + ", 4326))");
         params.push("POINT(" + req.query.lng + " " + req.query.lat + ")");
       }
-      /*if (req.query.rocktype) {
-        if (where.length < 1) {
-          where += " WHERE "
-        } else {
-          where += " AND "
-        }
-        var placeholder = "$" + (params.length + 1);
-        where += "(a.rocktype1 ILIKE " + placeholder + " OR a.rocktype2 ILIKE " + placeholder + " OR b.rocktype3 ILIKE " + placeholder + ")";
-        params.push("%" + req.query.rocktype3ype + "%");
-      }*/
 
       if (req.query.gid && req.query.gid != "undefined") {
-        if (where.length < 1) {
-          where += " WHERE "
-        } else {
-          where += " AND "
-        }
-        var placeholder = "$" + (params.length + 1);
-        where += " gid = " + placeholder;
+        where.push("gid = $" + (where.length + 1));
         params.push(req.query.gid);
       }
 
       if (req.query.unit_name) {
-        if (where.length < 1) {
-          where += " WHERE "
-        } else {
-          where += " AND "
-        }
-        var placeholder = "$" + (params.length + 1);
-        where += " unit_name ILIKE " + placeholder;
-        params.push("%" + req.query.unit_name + "%");
+        where.push("text_search @@ plainto_tsquery('macro', $" + (where.length + 1) + ")")
+        params.push(req.query.unit_name);
       }
 
       if (req.query.interval_name) {
-        if (where.length < 1) {
-          where += " WHERE "
-        } else {
-          where += " AND "
-        }
-        var placeholder = "$" + (params.length + 1);
-        where += " c.cmin_age = " + placeholder;
-        params.push(req.query.interval_name);
+        where.push("macro_b_age <= (SELECT age_bottom FROM macrostrat.intervals WHERE interval_name = $" + (where.length + 1) + ") AND macro_t_age <= (SELECT age_top FROM macrostrat.intervals WHERE interval_name = $" + (where.length + 2) + ")");
+        params.push(req.query.interval_name, req.query.interval_name);
       }
 
-      if (where === "" || params.length === 0) {
+      if (where.length === 0) {
         return larkin.error(req, res, next, "Invalid params");
       }
-      larkin.queryPg("earthbase", "SELECT a.unit_link, u_rocktype1 AS rt1, u_rocktype2 AS rt2, u_rocktype3 AS rt3, unit_name, unit_age, interval_color, c.cmin_age AS min_age, c.cmax_age AS max_age, lith1, lith2, lith3, lith4, lith5, unitdesc, unit_com, a.gid, string_agg(gm.unit_id::text, '|') AS macro_units" + ((geo) ? ", ST_AsGeoJSON(the_geom) AS geometry" : "") + " FROM gmus.geologic_units_with_intervals a LEFT JOIN gmus.lith b ON a.unit_link = b.unit_link LEFT JOIN gmus.age c ON a.unit_link = c.unit_link LEFT JOIN gmus.geounits_macrounits gm ON a.gid = gm.geologic_unit_gid" + where + " GROUP BY gm.geologic_unit_gid, a.unit_link, u_rocktype1, u_rocktype2, u_rocktype3, unit_name, unit_age, interval_color, c.cmin_age, c.cmax_age, lith1, lith2, lith3, lith4, lith5, unitdesc, unit_com, a.gid", params, function(error, result) {
+
+      larkin.queryPg("geomacro", "SELECT DISTINCT ON (gid) gid, macro_color AS interval_color, lith1, lith2, lith3, lith4, lith5, gm.best_units AS macro_units, i1.interval_name AS min_age, i2.interval_name as max_age, u_rocktype1 AS rt1, u_rocktype2 AS rt2, u_rocktype3 AS rt3, lu.interval_name AS unit_age, unit_com, lu.unit_link, unit_name, unitdesc, strat_unit" + ((geo) ? ", ST_AsGeoJSON(geom) AS geometry" : "") + " FROM gmus.lookup_units lu JOIN gmus.ages a ON lu.unit_link = a.unit_link JOIN macrostrat.intervals i1 ON a.macro_min_interval_id = i1.id JOIN macrostrat.intervals i2 ON a.macro_max_interval_id = i2.id LEFT JOIN gmus.liths l ON lu.unit_link = l.unit_link LEFT JOIN gmus.best_geounits_macrounits gm ON lu.gid = gm.geologic_unit_gid WHERE " + where.join(", "), params, function(error, result) {
         if (error) {
           callback(error);
         } else {
-          result.rows.forEach(function(d) {
-            d.macro_units = larkin.jsonifyPipes(d.macro_units, "integers");
-          });
           if (geo) {
             dbgeo.parse({
               "data": result.rows,
@@ -106,27 +75,20 @@ module.exports = function(req, res, next) {
 
 /* IF GMNA */
     } else if (req.query.type === "gmna") {
-      var where = "",
+      var where = [],
           params = [];
 
       if (req.query.lat && req.query.lng) {
-        var placeholder = "$" + (params.length + 1);
-        where += " WHERE ST_Contains(the_geom, ST_GeomFromText(" + placeholder + ", 4326))";
+        where.push(" ST_Contains(geom, ST_GeomFromText($" + (where.length + 1)+ ", 4326))");
         params.push("POINT(" + req.query.lng + " " + req.query.lat + ")");
       }
 
       if (req.query.interval_name) {
-        if (where.length < 1) {
-          where += " WHERE "
-        } else {
-          where += " AND "
-        }
-        var placeholder = "$" + (params.length + 1);
-        where += " b.interval_name ILIKE " + placeholder + " OR b.interval_name ILIKE " + placeholder;
-        params.push(req.query.interval_name);
+        where.push(" min_interval ILIKE $" + (where.length + 1) + " OR max_interval ILIKE $" + (where.length + 2));
+        params.push(req.query.interval_name, req.query.interval_name);
       }
 
-      larkin.queryPg("earthbase", "SELECT unit_abbre, rocktype, lithology, min_age, max_age, b.interval_name" + ((geo) ? ", ST_AsGeoJSON(the_geom) AS geometry" : "") + " FROM gmna.geologic_units a LEFT JOIN gmna.intervals_old b ON a.eb_interval_id = b.interval_id" + where, params, function(error, result) {
+      larkin.queryPg("geomacro", "SELECT unit_abbre, rocktype, lithology, min_interval AS min_age, max_interval AS max_age, containing_interval AS interval_name" + ((geo) ? ", ST_AsGeoJSON(geom) AS geometry" : "") + " FROM gmna.lookup_units WHERE " + where.join(", "), params, function(error, result) {
         if (error) {
           callback(error);
         } else {
