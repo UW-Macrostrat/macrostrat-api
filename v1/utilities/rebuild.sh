e52eb72fe82279e1eee565b176aa1573ae2b1149
John J Czaplewski <jczaplew@gmail.com>
2015-04-14 15:09:29 -0500

    Tweaks to build process

--- v1/utilities/rebuild.sh
+++ v1/utilities/rebuild.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python python scripts/build_lookup_unit_intervals.py && python scripts/build_lookup_strat_names.py && python scripts/build_lookup_unit_liths.py && python scripts/build_pbdb_matches.py && python scripts/fix_zeros.py
+python scripts/update_unit_boundaries.py && python scripts/build_lookup_unit_intervals.py && python scripts/build_lookup_strat_names.py && python scripts/build_lookup_unit_liths.py && python scripts/build_pbdb_matches.py && python scripts/fix_zeros.py
