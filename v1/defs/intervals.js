386a8f8f9af70a07620f87b5b182e6c96860bd5e
John J Czaplewski <jczaplew@gmail.com>
2015-05-15 09:54:08 -0500

    Update /defs/intervals to accept a timescale_id and always output timescale_id

--- v1/defs/intervals.js
+++ v1/defs/intervals.js
@@ -6,60 +6,81 @@ module.exports = function(req, res, next) {
     return larkin.info(req, res, next);
   }
 
-  var sql = "SELECT intervals.id, interval_name AS name, interval_abbrev abbrev, age_top AS late_age, age_bottom AS early_age, interval_type AS type, interval_color AS color FROM intervals",
+  var sql = "SELECT intervals.id, interval_name AS name, interval_abbrev abbrev, age_top AS late_age, age_bottom AS early_age, interval_type AS type, interval_color AS color, GROUP_CONCAT(DISTINCT timescales.id SEPARATOR '|') AS timescale_id FROM intervals JOIN timescales_intervals ON interval_id=intervals.id JOIN timescales ON timescale_id=timescales.id ",
       params = [];
 
   var where = 0;
+
   if (req.query.all) {
     // do nothing
   } else {
     if (req.query.timescale) {
-      sql += " JOIN timescales_intervals ON interval_id=intervals.id JOIN timescales ON timescale_id=timescales.id WHERE timescale = ?";
+      sql += " WHERE timescale = ?";
       params.push(req.query.timescale);
       where ++;
-    } 
+    } else if (req.query.timescale_id) {
+      sql += " WHERE timescales.id = ?";
+      params.push(req.query.timescale_id);
+      where ++;
+    }
+
     if (req.query.id && isFinite(req.query.id)) {
       if (where < 1) {
-        sql += " WHERE id = ?";
+        sql += " WHERE intervals.id = ?";
         params.push(req.query.id);
       } else {
-        sql += " AND id = ?";
+        sql += " AND intervals.id = ?";
         params.push(req.query.id);
       }
       where++;
     } 
+
     if (req.query.late_age && req.query.early_age) {
       if (req.query.rule === "loose") {
         if (where < 1) {
-          sql += " WHERE age_bottom >= ? AND age_top <= ?";
+          sql += " WHERE intervals.age_bottom >= ? AND intervals.age_top <= ?";
           params.push(req.query.late_age, req.query.early_age);
         } else {
-          sql += " AND (age_bottom >= ? AND age_top <= ?)";
+          sql += " AND (intervals.age_bottom >= ? AND intervals.age_top <= ?)";
           params.push(req.query.late_age, req.query.early_age);
         }
       } else {
         if (where < 1) {
-          sql += " WHERE age_top <= ? AND age_bottom >= ? OR age_top >= ? AND age_bottom <= ?";
+          sql += " WHERE intervals.age_top <= ? AND intervals.age_bottom >= ? OR intervals.age_top >= ? AND intervals.age_bottom <= ?";
           params.push(req.query.late_age, req.query.early_age, req.query.late_age, req.query.early_age);
         } else {
-          sql += " AND ((age_top <= ? AND age_bottom >= ?) OR (age_top >= ? AND age_bottom <= ?))";
+          sql += " AND ((intervals.age_top <= ? AND intervals.age_bottom >= ?) OR (intervals.age_top >= ? AND intervals.age_bottom <= ?))";
           params.push(req.query.late_age, req.query.early_age, req.query.late_age, req.query.early_age);
         }
       }
         
     } else if (req.query.age) {
       if (where < 1) {
-        sql += " WHERE age_top <= ? AND age_bottom >= ?";
+        sql += " WHERE intervals.age_top <= ? AND intervals.age_bottom >= ?";
         params.push(req.query.age, req.query.age);
       } else {
-        sql += " AND age_top <= ? AND age_bottom >= ?";
+        sql += " AND intervals.age_top <= ? AND intervals.age_bottom >= ?";
         params.push(req.query.age, req.query.age);
       }
     }
   }
   
-  sql += " ORDER BY late_age ASC";
-  var format = (api.acceptedFormats.standard[req.query.format]) ? req.query.format : "json";
+  sql += " GROUP BY intervals.id ORDER BY late_age ASC";
 
-  larkin.query(sql, params, null, true, res, format, next);
+
+  larkin.query(sql, params, function(error, result) {
+    if (error) {
+      console.log(error);
+      return larkin.error(req, res, next, "Something went wrong");
+    } else {
+      if (req.query.format !== "csv") {
+        result.forEach(function(d) {
+          d.timescale_id = larkin.jsonifyPipes(d.timescale_id, "integers");
+        });
+      }
+
+      larkin.sendData(result, res, (api.acceptedFormats.standard[req.query.format]) ? req.query.format : "json", next);
+    }
+  });
+
 }
