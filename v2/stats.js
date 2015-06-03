e5c40a90908c51ab63f7fca66de8976450b333e5
Shanan Peters <cambro@gmail.com>
2015-06-03 10:29:55 -0500

    Update stats.js
    added columns and project_id, by status_code

--- v2/stats.js
+++ v2/stats.js
@@ -7,12 +7,12 @@ module.exports = function(req, res, next) {
   }
 
   var sql = "\
-    SELECT project, COUNT(distinct units_sections.section_id) AS packages, COUNT(distinct unit_id) AS units, COUNT(distinct collection_no) AS pbdb_collections FROM units_sections \
+    SELECT project_id, project, status_code status, COUNT(distinct units_sections.col_id) AS colums, COUNT(distinct units_sections.section_id) AS packages, COUNT(distinct unit_id) AS units, COUNT(distinct collection_no) AS pbdb_collections FROM units_sections \
         JOIN cols ON cols.id = col_id \
         JOIN projects ON projects.id = project_id \
         LEFT JOIN pbdb_matches USING (unit_id) \
-        WHERE project IN ('North America','New Zealand','Caribbean','Deep Sea') and status_code='active' \
-        GROUP BY project_id";
+        WHERE project IN ('North America','New Zealand','Caribbean','Deep Sea') \
+        GROUP BY project_id, status_code";
 
   var format = (api.acceptedFormats.standard[req.query.format]) ? req.query.format : "json";
 
