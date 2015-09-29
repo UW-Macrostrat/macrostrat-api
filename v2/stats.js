4d2fee48930c901f3e69ae91d39d35c5fa772d94
John J Czaplewski <jczaplew@gmail.com>
2015-09-29 15:00:12 -0500

    Add lookup table for stats

--- v2/stats.js
+++ v2/stats.js
@@ -8,14 +8,15 @@ module.exports = function(req, res, next) {
   }
 
   var sql = multiline(function() {/*
-    SELECT project_id, project, status_code status, COUNT(distinct units_sections.col_id) AS columns, COUNT(distinct units_sections.section_id) AS packages, COUNT(distinct unit_id) AS units, COUNT(distinct collection_no) AS pbdb_collections, COUNT(unit_measures.id) AS measurements
-    FROM units_sections
-    JOIN cols ON cols.id = col_id
-    JOIN projects ON projects.id = project_id
-    LEFT JOIN pbdb_matches USING (unit_id)
-    LEFT JOIN unit_measures USING (unit_id)
-    WHERE project IN ('North America','New Zealand','Caribbean','Deep Sea') and status_code='active'
-    GROUP BY project_id, status_code;
+    SELECT
+      project_id,
+      project,
+      columns,
+      packages,
+      units,
+      pbdb_collections,
+      measurements
+    FROM stats
   */});
 
 
