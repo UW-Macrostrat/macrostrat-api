9d547443402dae10254e37ae634b350a3bd98f4b
John J Czaplewski <jczaplew@gmail.com>
2015-09-30 17:08:45 -0500

    Implement tile server within API

--- v2/api.js
+++ v2/api.js
@@ -1,6 +1,13 @@
 var express = require("express");
+var tilestrata = require('tilestrata');
+var disk = require('tilestrata-disk');
+var sharp = require('tilestrata-sharp');
+var mapnik = require('tilestrata-mapnik');
+var dependency = require('tilestrata-dependency');
+var credentials = require("./credentials");
 
 var api = express.Router();
+var strata = tilestrata.createServer();
 
 api.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -8,6 +15,26 @@ api.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
   next();
 });
+
+strata.layer("burwell")
+    .route("tile@2x.png")
+        .use(disk.cache({dir: credentials.tiles.path}))
+        .use(mapnik({
+            xml: credentials.tiles.config,
+            tileSize: 512,
+            scale: 2
+        }))
+    .route("tile.png")
+        .use(disk.cache({dir: credentials.tiles.path}))
+        .use(dependency("burwell", "tile@2x.png"))
+        .use(sharp(function(image, sharp) {
+            return image.resize(256);
+        }));
+
+api.use(tilestrata.middleware({
+  server: strata,
+  prefix: "/tiles"
+}))
 
 api.acceptedFormats = {
   "standard": {
