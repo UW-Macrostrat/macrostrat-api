02935cd00459d7e6cf5513bf83336b7b09defcda
Shanan Peters <cambro@gmail.com>
2016-02-16 08:56:48 -0600

    meta expansion

--- v2/meta.js
+++ v2/meta.js
@@ -7,13 +7,22 @@ module.exports = function(req, res, next) {
     bare: (api.acceptedFormats.bare[req.query.format]) ? true : false
   }, {
     data: {
-      "license": "All data is licensed under Creative Commons BY 4.0 International",
-      "citation": "",
-      "authors": [
+      "license": "All data are licensed under Creative Commons BY 4.0 International",
+      "citation": "Peters, S.E. et al. Macrostrat: a platform for geological data integration and deep-time research. https://macrostrat.org, accessed on (date)",
+      "api": [
         "John J Czaplewski",
-        "Shanan E Peters",
-        "Puneet Kishor"
+        "Puneet Kishor",
+        "Shanan E Peters"
       ]
+    },
+    support: {
+      "current_funding": "U.S. National Science Foundation Grant EAR-1150082 and ICER-1440312. Additional support from the University of Wisconsin-Madison Dept. of Geoscience.",
+      "previous_funding": "Initial database development supported by the United States Geological Survey and the American Chemical Society."
+    }
+    acknowledgements: {
+      "data_entry": "University of Michigan Undergraduate Research Opportunity program, Noel Heim, Deb Rook, Neal Auchter, Annaka Clement, Clay Kelly.",
+      "contributors": "Jon Husson",
+      "data_providers": "The data synthesized in Macrostrat reflect the combined work of hundreds of field-based geoscientists. These contributors are too many to cite individually, but see /defs/refs?all for a list of primary sources supplying data and the references therein."
     }
   });
 
