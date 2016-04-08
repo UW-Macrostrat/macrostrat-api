b9f19f6fc0ef5d93b86a755e70c28bfc360497fa
John J Czaplewski <jczaplew@gmail.com>
2016-04-08 10:38:33 -0500

    Clean up mineral regex

--- v2/defs/minerals.js
+++ v2/defs/minerals.js
@@ -14,15 +14,22 @@ module.exports = function(req, res, next, cb) {
     sql += " WHERE minerals.id IN (:mineral_id)";
     params["mineral_id"] = larkin.parseMultipleIds(req.query.mineral_id);
   } else if (req.query.mineral) {
-    sql += " WHERE mineral IN  (:mineral)";
+    sql += " WHERE mineral IN (:mineral)";
     params["mineral"] = larkin.parseMultipleStrings(req.query.mineral);
   } else if (req.query.mineral_type){
     sql += " WHERE min_type IN (:mineral_type)";
     params["mineral_type"] = larkin.parseMultipleStrings(req.query.mineral_type);
   } else if (req.query.element) {
-    req.query.element = larkin.pool.escapeId(larkin.parseMultipleStrings(req.query.element).join('|'));
 
-    sql += " WHERE REGEXP_INSTR(BINARY formula, '.?" + req.query.element + "[[:upper:]]+') or REGEXP_INSTR(BINARY formula, '.?" + req.query.element + "[[:digit:]]+') or REGEXP_INSTR(BINARY formula, '.?[" + req.query.element + "][\\(|\\),x,\\+,\\[,\\],-]') or REGEXP_INSTR(BINARY formula, '.?" + req.query.element + "$') ORDER BY mineral";
+    var parsedElements = larkin.parseMultipleStrings(req.query.element).join('|');
+    params = {
+      elementA: ".*" + parsedElements + "[A-Z]+",
+      elementB: ".*" + parsedElements + "[0-9]+",
+      elementC: ".*[" + parsedElements + "][\\(|\\),x,\\+,\\[,\\],-]",
+      elementD: ".*" + parsedElements + "$"
+    }
+
+    sql += " WHERE formula REGEXP BINARY :elementA OR formula REGEXP BINARY :elementB OR formula REGEXP BINARY :elementC OR formula REGEXP BINARY :elementD ORDER BY mineral";
 
   }
 
