570b730a9af2ce9898ce2661b38912fff07e0fd4
Shanan Peters <cambro@gmail.com>
2016-08-15 11:50:22 -0500

    lookup build script for occurrences

--- v2/utilities/scripts/build_lookup_units.py
+++ v2/utilities/scripts/build_lookup_units.py
@@ -57,7 +57,7 @@ cursor = connection.cursor()
 
 # Initial query to populate the lookup table
 cursor.execute("""
-    INSERT INTO lookup_units_new (unit_id, col_area, project_id, t_int, t_int_name, t_int_age, t_age, t_prop, t_plat, t_plng, b_int, b_int_name, b_int_age, b_age, b_prop, b_plat, b_plng, clat,  clng, color, text_color, units_above, units_below, pbdb_collections)
+    INSERT INTO lookup_units_new (unit_id, col_area, project_id, t_int, t_int_name, t_int_age, t_age, t_prop, t_plat, t_plng, b_int, b_int_name, b_int_age, b_age, b_prop, b_plat, b_plng, clat,  clng, color, text_color, units_above, units_below, pbdb_collections, pbdb_occurrences)
     SELECT
       units.id AS unit_id,
       cols.col_area,
