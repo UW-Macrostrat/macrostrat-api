628624109e30f9d62f45dde339cece55435a663f
John J Czaplewski <jczaplew@gmail.com>
2016-09-29 17:21:45 -0500

    Have mobile point use burwell

--- v2/mobile/point.js
+++ v2/mobile/point.js
@@ -2,29 +2,105 @@ var api = require("../api"),
     async = require("async"),
     larkin = require("../larkin");
 
+var _ = require("underscore")
+
+var priorities = {
+  'tiny': ['tiny', 'small', 'medium', 'large'],
+  'small': ['small', 'medium', 'large', 'tiny'],
+  'medium': ['medium', 'large', 'small', 'tiny'],
+  'large': ['large', 'medium', 'small', 'tiny']
+}
+
+function buildSQL(scale, where) {
+  return `
+    (SELECT
+      m.map_id,
+      m.source_id,
+      COALESCE(m.name, '') AS name,
+      COALESCE(m.strat_name, '') AS strat_name,
+      COALESCE(m.lith, '') AS lith,
+      COALESCE(m.descrip, '') AS descrip,
+      COALESCE(m.comments, '') AS comments,
+      COALESCE(mm.unit_ids, '{}') AS macro_units,
+      COALESCE(mm.strat_name_ids, '{}') AS strat_names,
+      COALESCE(mm.lith_ids, '{}') AS liths,
+      array((SELECT lith FROM macrostrat.liths WHERE id =  ANY(lith_ids))) AS liths_full,
+      m.t_interval AS t_int_id,
+      ti.age_top::float AS t_int_age,
+      ti.interval_name AS t_int_name,
+      m.b_interval AS b_int_id,
+      tb.age_bottom::float AS b_int_age,
+      tb.interval_name AS b_int_name,
+      mm.color,
+      '${scale}' AS scale,
+      (SELECT row_to_json(r) FROM (SELECT
+        source_id,
+        name,
+        COALESCE(url, '') url,
+        COALESCE(ref_title, '') ref_title,
+        COALESCE(authors, '') authors,
+        COALESCE(ref_year, '') ref_year,
+        COALESCE(ref_source, '') ref_source,
+        COALESCE(isbn_doi, '') isbn_doi
+        FROM maps.sources
+        WHERE source_id = m.source_id) r)::jsonb AS ref
+      FROM maps.${scale} m
+      LEFT JOIN macrostrat.intervals ti ON m.t_interval = ti.id
+      LEFT JOIN macrostrat.intervals tb ON m.b_interval = tb.id
+      LEFT JOIN lookup_${scale} mm ON mm.map_id = m.map_id
+      ${where}
+    )
+  `
+}
+
 module.exports = function(req, res, next) {
   if (req.query.lat && req.query.lng) {
     async.parallel({
-      gmus: function(callback) {
-        larkin.queryPg("geomacro", "SELECT gid, state, (CASE WHEN (rocktype1 IS NULL) THEN '' ELSE rocktype1 END) AS rocktype1, (CASE WHEN (rocktype2 IS NULL) THEN '' ELSE rocktype2 END) AS rocktype2, (CASE WHEN (u_rocktype3 IS NULL) THEN '' ELSE u_rocktype3 END) AS rocktype3, unit_name, unit_age, unitdesc, strat_unit, unit_com FROM gmus.lookup_units WHERE ST_Contains(geom, ST_GeomFromText($1, 4326))", ["POINT(" + req.query.lng + " " + req.query.lat + ")"], function(error, result) {
-          if (error) {
-            callback(error);
-          } else {
-            callback(null, result.rows[0]);
-          }
-        });
-      },
+      burwell: function(callback) {
+        var where = []
+        var params = []
+        where.push('ST_Intersects(m.geom, ST_GeomFromText($' + (where.length + 1) + ', 4326))');
+        params.push(`SRID=4326;POINT(${req.query.lng} ${req.query.lat})`);
 
-      gmna: function(callback) {
-        larkin.queryPg("geomacro", "SELECT gid AS id, unit_abbre, rocktype, lithology, containing_interval AS interval_name, map_unit_n FROM gmna.lookup_units WHERE ST_Contains(geom, ST_GeomFromText($1, 4326))", ["POINT(" + req.query.lng + " " + req.query.lat + ")"], function(error, result) {
-          if (error) {
-            callback(error);
-          } else {
-            callback(null, result.rows[0]);
-          }
-        });
-      },
+        where = ' WHERE ' + where.join(' AND ')
 
+        var scaleSQL = Object.keys(priorities).map(function(scale) {
+          return buildSQL(scale, where);
+        }).join(' UNION ');
+
+        var toRun = `SELECT * FROM ( ${scaleSQL} ) doit`;
+
+        larkin.queryPg("burwell", toRun, params, function(error, result) {
+          if (error || !result.rows || !result.rows.length) {
+            return callback(null, {})
+          }
+          var currentScale = "large"
+          var returnedScales = _.uniq(result.rows.map(function(d) { return d.scale }))
+
+          var targetScales = []
+
+          // Iterate on possible scales given our z
+          for (var i = 0; i < priorities[currentScale].length; i++) {
+            // If that scale is present, record it
+             if (returnedScales.indexOf(priorities[currentScale][i]) > -1) {
+               targetScales.push(priorities[currentScale][i])
+               if (currentScale != 'tiny' && currentScale != 'small') {
+                 break
+               } else if (targetScales.length > 1) {
+                 break
+               }
+             }
+           }
+
+           var bestFit = result.rows.filter(function(d) {
+             if (targetScales.indexOf(d.scale) > -1) {
+               delete d.scale
+               return d
+             }
+           })[0]
+           callback(null, bestFit)
+        })
+      },
       // Query Macrostrat for polygon
       column: function(callback) {
         larkin.queryPg("geomacro", "SELECT id AS col_id FROM macrostrat.cols WHERE ST_Contains(poly_geom, ST_GeomFromText($1, 4326)) and status_code='active'", ["POINT(" + req.query.lng + " " + req.query.lat + ")"], function(error, result) {
@@ -55,13 +131,13 @@ module.exports = function(req, res, next) {
         larkin.error(req, res, next, error);
       } else {
         var response = {
-          "uid": (result.gmus && result.gmus.gid) ? result.gmus.gid : (result.gmna && result.gmna.id) ? result.gmna.id : "",
-          "rocktype": (result.gmus && result.gmus.rocktype1) ? [result.gmus.rocktype1, result.gmus.rocktype2, result.gmus.rocktype3].filter(function(d) { if (d) return d }) : (result.gmna && (result.gmna.rocktype || result.gmna.lithology)) ? [result.gmna.rocktype, result.gmna.lithology].filter(function(d) { if (d) return d }) : [],
-          "age": (result.gmus && result.gmus.unit_age) ? result.gmus.unit_age : (result.gmna && result.gmna.interval_name) ? result.gmna.interval_name : "",
-          "name": (result.gmus && result.gmus.unit_name) ? result.gmus.unit_name : (result.gmna && result.gmna.rocktype) ? result.gmna.rocktype + ((result.gmna.lithology) ? ", " + result.gmna.lithology : "") : "",
-          "desc": (result.gmus && result.gmus.unitdesc) ? result.gmus.unitdesc : "",
-          "comm": (result.gmus && result.gmus.unit_com) ? result.gmus.unit_com : (result.gmna && result.gmna.map_unit_n) ? result.gmna.map_unit_n : "",
-          "strat_unit": (result.gmus && result.gmus.strat_unit) ? result.gmus.strat_unit : "",
+          "uid": result.burwell.map_id || "",
+          "rocktype": result.burwell.liths_full || [],
+          "age": (result.burwell.b_int_name && result.burwell.t_int_name && result.burwell.b_int_name === result.burwell.t_int_name) ? result.burwell.b_int_name : result.burwell.b_int_name + ' - ' + result.burwell.t_int_name,
+          "name": result.burwell.name || "",
+          "desc": result.burwell.descrip || "",
+          "comm": result.burwell.comments || "",
+          "strat_unit": result.burwell.strat_name || "",
           "col_id": (result.column && result.column.col_id) ? result.column.col_id : ""
         };
 
