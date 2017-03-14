c4a7d33b96e0554478ef2dfd3ed0f174d0f641d2
John J Czaplewski <john@czaplewski.org>
2017-03-14 12:05:38 -0500

    Speed up and increase accuracy of nearby burwell

--- v2/geologic_units_burwell_nearby.js
+++ v2/geologic_units_burwell_nearby.js
@@ -5,212 +5,100 @@ var api = require("./api"),
 // To whom it may concern: sorry this is so hairy. Magic ain't cheap.
 var sql = {
   lithologies: `
-  WITH nn AS (
-    SELECT lith_id, lith, lith_type, lith_class, lith_color, distance
-    FROM (
-      (SELECT lith_id, liths.lith, lith_type, lith_class, lith_color, geom, ST_Distance(geom, st_setsrid(st_makepoint($1, $2),4326)) AS distance
-       FROM maps.medium m
-       LEFT JOIN maps.map_liths ON m.map_id = map_liths.map_id
-       LEFT JOIN macrostrat.liths ON liths.id = map_liths.lith_id
-       WHERE liths.lith IS NOT NULL
-       ORDER BY geom <#> st_setsrid(st_makepoint($1,$2),4326)
-       LIMIT 1000)
-      UNION ALL
-      (SELECT lith_id, liths.lith, lith_type, lith_class, lith_color, geom, ST_Distance(geom, st_setsrid(st_makepoint($1, $2),4326)) AS distance
-       FROM maps.small m
-       LEFT JOIN maps.map_liths ON m.map_id = map_liths.map_id
-       LEFT JOIN macrostrat.liths ON liths.id = map_liths.lith_id
-       WHERE liths.lith IS NOT NULL
-       ORDER BY geom <#> st_setsrid(st_makepoint($1,$2),4326)
-       LIMIT 1000)
-      UNION ALL
-      (SELECT lith_id, liths.lith, lith_type, lith_class, lith_color, geom, ST_Distance(geom, st_setsrid(st_makepoint($1, $2),4326)) AS distance
-       FROM maps.large m
-       LEFT JOIN maps.map_liths ON m.map_id = map_liths.map_id
-       LEFT JOIN macrostrat.liths ON liths.id = map_liths.lith_id
-       WHERE liths.lith IS NOT NULL
-       ORDER BY geom <#> st_setsrid(st_makepoint($1,$2),4326)
-       LIMIT 1000)
-    ) a
-    ORDER BY distance
-    LIMIT 1000
-  ),
-
-  nnlimit AS (
-    SELECT DISTINCT ON (lith_id) lith_id, lith, lith_type, lith_class, lith_color, distance
-    FROM nn
-    ORDER BY lith_id, distance
-  )
-
-  SELECT lith_id, lith AS name, lith_type AS type, lith_class AS class, lith_color AS color
-  FROM nnlimit
-  ORDER BY distance
-  LIMIT 5;`,
+    WITH first AS (
+      SELECT DISTINCT ON (lith_id) lith_id, distance
+      FROM (
+        SELECT lith_id, ST_Distance(geom, st_setsrid(st_makepoint($1, $2),4326)) AS distance
+        FROM maps.medium m
+        JOIN maps.map_liths ON m.map_id = map_liths.map_id
+        ORDER BY geom <-> st_setsrid(st_makepoint($1, $2),4326)
+        LIMIT 100
+      ) a
+      ORDER BY lith_id, distance
+    )
+    SELECT lith_id, lith AS name, lith_type AS type, lith_class AS class, lith_color AS color
+    FROM first
+    JOIN macrostrat.liths ON liths.id = first.lith_id
+    ORDER BY distance ASC
+    LIMIT 10`,
 
   strat_names: `
-  WITH nn AS (
-    SELECT strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age, t_age, b_period, t_period, geom, row_number() OVER() as row_number
-    FROM (
-      SELECT strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age, t_age, b_period, t_period, geom FROM (
-        SELECT msn.strat_name_id, lookup_strat_names.strat_name, lookup_strat_names.rank_name AS strat_name_long, bed_name AS bed, mbr_name AS mbr, fm_name AS fm, gp_name AS gp, sgp_name AS sgp, early_age AS b_age, late_age AS t_age, b_period, t_period, geom
-         FROM maps.large m
-         LEFT JOIN maps.map_strat_names msn ON m.map_id = msn.map_id
-         LEFT JOIN macrostrat.lookup_strat_names ON lookup_strat_names.strat_name_id = msn.strat_name_id
-         WHERE msn.strat_name_id IS NOT NULL
-         ORDER BY geom <#> st_setsrid(st_makepoint($1, $2),4326)
+    SELECT strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age, t_age, b_period, t_period FROM (
+      SELECT DISTINCT ON (strat_name_id) strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age, t_age, b_period, t_period, geom, distance FROM (
+        SELECT
+          msn.strat_name_id,
+          lookup_strat_names.strat_name,
+          lookup_strat_names.rank_name AS strat_name_long,
+          bed_name AS bed,
+          mbr_name AS mbr,
+          fm_name AS fm,
+          gp_name AS gp,
+          sgp_name AS sgp,
+          early_age AS b_age,
+          late_age AS t_age,
+          b_period, t_period,
+          geom,
+          ST_Distance(geom, st_setsrid(st_makepoint($1, $2),4326)) AS distance
+         FROM maps.:scale m
+         JOIN maps.map_strat_names msn ON m.map_id = msn.map_id
+         JOIN macrostrat.lookup_strat_names ON lookup_strat_names.strat_name_id = msn.strat_name_id
+         ORDER BY geom <-> st_setsrid(st_makepoint($1, $2),4326)
          LIMIT 100
       ) a
-      UNION ALL
-      SELECT strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age, t_age, b_period, t_period, geom FROM (
-        SELECT msn.strat_name_id, lookup_strat_names.strat_name, lookup_strat_names.rank_name AS strat_name_long, bed_name AS bed, mbr_name AS mbr, fm_name AS fm, gp_name AS gp, sgp_name AS sgp, early_age AS b_age, late_age AS t_age, b_period, t_period, geom
-         FROM maps.medium m
-         LEFT JOIN maps.map_strat_names msn ON m.map_id = msn.map_id
-         LEFT JOIN macrostrat.lookup_strat_names ON lookup_strat_names.strat_name_id = msn.strat_name_id
-         WHERE msn.strat_name_id IS NOT NULL
-         ORDER BY geom <#> st_setsrid(st_makepoint($1, $2),4326)
-         LIMIT 100
-      ) b
-      UNION ALL
-      SELECT strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age, t_age, b_period, t_period, geom FROM (
-        SELECT msn.strat_name_id, lookup_strat_names.strat_name, lookup_strat_names.rank_name as strat_name_long, bed_name AS bed, mbr_name AS mbr, fm_name AS fm, gp_name AS gp, sgp_name AS sgp, early_age AS b_age, late_age AS t_age, b_period, t_period, geom
-         FROM maps.small m
-         LEFT JOIN maps.map_strat_names msn ON m.map_id = msn.map_id
-         LEFT JOIN macrostrat.lookup_strat_names ON lookup_strat_names.strat_name_id = msn.strat_name_id
-         WHERE msn.strat_name_id IS NOT NULL
-         ORDER BY geom <#> st_setsrid(st_makepoint($1, $2),4326)
-         LIMIT 100
-      ) c
-    )a
-    ORDER BY geom <#> st_setsrid(st_makepoint($1, $2),4326)
-    LIMIT 100
-  ),
-
-  nnlimit AS (
-    SELECT DISTINCT ON (strat_name_id) strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age, t_age, b_period, t_period, row_number
-    FROM nn
-  )
-
-  SELECT strat_name_id, strat_name, strat_name_long, bed, mbr, fm, gp, sgp, b_age::float, t_age::float, b_period, t_period
-  FROM nnlimit
-  ORDER BY row_number
-  LIMIT 5;`,
+      ORDER BY strat_name_id, distance
+    ) b
+    WHERE strat_name_id IN (
+      SELECT strat_name_id
+      FROM macrostrat.strat_name_footprints
+      WHERE ST_Intersects(geom, st_setsrid(st_makepoint($1, $2),4326))
+    )
+    ORDER BY distance;
+  `,
 
   intervals: `
-    WITH nn AS (
-      SELECT interval_id, row_number() OVER() AS row_number
-      FROM (
-        SELECT map_id, t_interval as interval_id, geom from maps.large
-        UNION ALL
-        SELECT map_id, t_interval as interval_id, geom from maps.medium
-        UNION ALL
-        SELECT map_id, t_interval as interval_id, geom from maps.small
-      ) a
-      ORDER BY geom <-> st_setsrid(st_makepoint($1,$2),4326)
+  WITH first AS (
+    SELECT DISTINCT ON (interval_id) interval_id, distance
+    FROM (
+      SELECT
+        map_id,
+        t_interval as interval_id,
+        geom,
+        ST_Distance(geom, st_setsrid(st_makepoint($1, $2),4326)) AS distance
+      FROM maps.:scale
+      ORDER BY geom <-> st_setsrid(st_makepoint($1, $2),4326)
       LIMIT 1000
-    ),
-
-    nnlimit AS (
-      SELECT DISTINCT ON (interval_id) interval_id, row_number
-      FROM nn
-      ORDER BY interval_id, row_number
-    )
-
-    SELECT interval_id AS int_id, interval_name AS name, age_bottom AS b_age, age_top AS t_age, interval_color AS color
-    FROM nnlimit
-    JOIN macrostrat.intervals on nnlimit.interval_id = intervals.id
-    ORDER BY row_number
-    LIMIT 5;`,
+     ) a
+     ORDER BY interval_id, distance
+  )
+  SELECT interval_id AS int_id, interval_name AS name, age_bottom AS b_age, age_top AS t_age, interval_color AS color
+  FROM first
+  JOIN macrostrat.intervals on first.interval_id = intervals.id
+  ORDER BY distance ASC
+  LIMIT 5;`,
 
   map_units: `
-    WITH subset AS (
-      SELECT mm.map_id, name, 'tiny' AS scale, geom,
-      (SELECT COALESCE(interval_name, '')
-         FROM macrostrat.intervals
-         JOIN macrostrat.timescales_intervals ON intervals.id = timescales_intervals.interval_id
-         JOIN macrostrat.timescales ON timescales_intervals.timescale_id = timescales.id
-         WHERE age_top <= mm.best_age_top::float AND age_bottom >= mm.best_age_bottom::float
-         AND timescales.id IN (11,14)
-         ORDER BY age_bottom - age_top
-         LIMIT 1
-      ) AS int_name
-      FROM maps.tiny
-      JOIN lookup_tiny mm on tiny.map_id = mm.map_id
-      WHERE ST_Intersects(geom, ST_Buffer($1::geography, 3200)::geometry)
-      UNION ALL
-      SELECT mm.map_id, name, 'small' AS scale, geom,
-      (SELECT COALESCE(interval_name, '')
-         FROM macrostrat.intervals
-         JOIN macrostrat.timescales_intervals ON intervals.id = timescales_intervals.interval_id
-         JOIN macrostrat.timescales ON timescales_intervals.timescale_id = timescales.id
-         WHERE age_top <= mm.best_age_top::float AND age_bottom >= mm.best_age_bottom::float
-         AND timescales.id IN (11,14)
-         ORDER BY age_bottom - age_top
-         LIMIT 1
-      ) AS int_name
-      FROM maps.small
-      JOIN lookup_small mm on small.map_id = mm.map_id
-      WHERE ST_Intersects(geom, ST_Buffer($1::geography, 3200)::geometry)
-      UNION ALL
-      SELECT mm.map_id, name, 'medium' AS scale, geom,
-      (SELECT COALESCE(interval_name, '')
-         FROM macrostrat.intervals
-         JOIN macrostrat.timescales_intervals ON intervals.id = timescales_intervals.interval_id
-         JOIN macrostrat.timescales ON timescales_intervals.timescale_id = timescales.id
-         WHERE age_top <= mm.best_age_top::float AND age_bottom >= mm.best_age_bottom::float
-         AND timescales.id IN (11,14)
-         ORDER BY age_bottom - age_top
-         LIMIT 1
-      ) AS int_name
-      FROM maps.medium
-      JOIN lookup_medium mm on medium.map_id = mm.map_id
-      WHERE ST_Intersects(geom, ST_Buffer($1::geography, 3200)::geometry)
-      UNION ALL
-      SELECT mm.map_id, name, 'large' AS scale, geom,
-      (SELECT COALESCE(interval_name, '')
-         FROM macrostrat.intervals
-         JOIN macrostrat.timescales_intervals ON intervals.id = timescales_intervals.interval_id
-         JOIN macrostrat.timescales ON timescales_intervals.timescale_id = timescales.id
-         WHERE age_top <= mm.best_age_top::float AND age_bottom >= mm.best_age_bottom::float
-         AND timescales.id IN (11,14)
-         ORDER BY age_bottom - age_top
-         LIMIT 1
-      ) AS int_name
-      FROM maps.large
-      JOIN lookup_large mm on large.map_id = mm.map_id
-      WHERE ST_Intersects(geom, ST_Buffer($1::geography, 3200)::geometry)
-    ),
-    best_scale AS (
-      SELECT
-        CASE WHEN 'large' IN (SELECT DISTINCT scale FROM subset)
-          THEN ARRAY['large', 'medium']
-        WHEN 'medium' IN (SELECT DISTINCT scale FROM subset)
-          THEN ARRAY['medium', 'small']
-        WHEN 'small' IN (SELECT DISTINCT scale FROM subset)
-          THEN ARRAY['small', 'tiny']
-        WHEN 'tiny' IN (SELECT DISTINCT scale FROM subset)
-          THEN ARRAY['tiny']
-        ELSE null
-       END AS best
-    ),
-    units AS (
-      SELECT first_value(map_id) OVER (
-        PARTITION BY name
-        ORDER BY ST_Distance(geom, $1)
-      ) map_id, first_value(name) OVER (
-        PARTITION BY name
-        ORDER BY ST_Distance(geom, $1)
-      ) unit_name, ST_Distance(geom::geography, $1::geography)::int distance, int_name
-      FROM subset
-      WHERE ST_Intersects(geom, ST_Buffer($1::geography, 3200)::geometry)
-      AND name NOT ILIKE '%water%' AND (SELECT scale = ANY(best) FROM best_scale)
-      GROUP BY map_id, name, geom, int_name
-      ORDER BY distance
-    )
-
-    SELECT map_id, unit_name, min(distance) AS distance, int_name
-    FROM units
-    GROUP BY map_id, unit_name, int_name
-    ORDER BY min(distance)
+  SELECT map_id, name AS unit_id, distance, int_name
+  FROM (
+  SELECT DISTINCT ON (name) map_id, name, int_name, distance
+  FROM (
+    SELECT mm.map_id, name, ST_Distance(geom::geography, st_setsrid(st_makepoint($1, $2), 4326)::geography)::int AS distance,
+    (SELECT COALESCE(interval_name, '')
+       FROM macrostrat.intervals
+       JOIN macrostrat.timescales_intervals ON intervals.id = timescales_intervals.interval_id
+       JOIN macrostrat.timescales ON timescales_intervals.timescale_id = timescales.id
+       WHERE age_top <= mm.best_age_top::float AND age_bottom >= mm.best_age_bottom::float
+       AND timescales.id IN (11,14) AND name NOT ILIKE '%water%'
+       ORDER BY age_bottom - age_top
+       LIMIT 1
+    ) AS int_name
+    FROM maps.:scale m
+    JOIN lookup_:scale mm on m.map_id = mm.map_id
+    ORDER BY geom <-> st_setsrid(st_makepoint($1, $2),4326)
+    LIMIT 200
+  ) a
+  ORDER BY name, distance
+  ) b ORDER BY distance
+  LIMIT 10
   `,
 
   // places: `
@@ -238,71 +126,119 @@ module.exports = function(req, res, next) {
     return;
   }
 
-  req.query.lng = parseFloat(req.query.lng).toFixed(4)
+  req.query.lng = larkin.normalizeLng(parseFloat(req.query.lng).toFixed(4))
   req.query.lat = parseFloat(req.query.lat).toFixed(4)
 
-  async.parallel({
-    strat_names: function(callback) {
-      larkin.queryPg("burwell", sql.strat_names, [req.query.lng, req.query.lat], function(error, data) {
-        if (error) {
-          callback(error);
-        } else {
-          callback(null, data.rows);
-        }
-      });
-    },
+  async.waterfall([
+    function(cb) {
+      larkin.queryPg("burwell", `
+      SELECT
+        CASE
+          WHEN (
+            SELECT
+              CASE
+                WHEN count(*) > 0 THEN True
+                ELSE False
+               END
+            FROM maps.large
+            WHERE ST_Intersects(geom, ST_SetSRID(ST_MakePoint($1, $2),4326))
+          ) THEN 'large'
+          WHEN (
+            SELECT
+              CASE
+                WHEN count(*) > 0 THEN True
+                ELSE False
+               END
+            FROM maps.medium
+            WHERE ST_Intersects(geom, ST_SetSRID(ST_MakePoint($1, $2),4326))
+          ) THEN 'medium'
+          WHEN (
+            SELECT
+              CASE
+                WHEN count(*) > 0 THEN True
+                ELSE False
+               END
+            FROM maps.small
+            WHERE ST_Intersects(geom, ST_SetSRID(ST_MakePoint($1, $2),4326))
+          ) THEN 'small'
+          ELSE ''
+        END AS scale
+      `, [ req.query.lng, req.query.lat ], function(error, result) {
+        if (error || !result.rows) return cb(error)
+        cb(null, result.rows[0].scale)
+      })
+    }, function(scale, cb) {
+      if (scale === '') {
+        return cb(null, [])
+      }
+      async.parallel({
+        strat_names: function(callback) {
+          larkin.queryPg("burwell", sql.strat_names.replace(':scale', scale), [req.query.lng, req.query.lat], function(error, data) {
+            if (error) {
+              callback(error);
+            } else {
+              callback(null, data.rows);
+            }
+          });
+        },
 
-    lithologies: function(callback) {
-      larkin.queryPg("burwell", sql.lithologies, [req.query.lng, req.query.lat], function(error, data) {
-        if (error) {
-          callback(error);
-        } else {
-          callback(null, data.rows);
-        }
-      });
-    },
+        lithologies: function(callback) {
+          larkin.queryPg("burwell", sql.lithologies.replace(':scale', scale), [req.query.lng, req.query.lat], function(error, data) {
+            if (error) {
+              callback(error);
+            } else {
+              callback(null, data.rows);
+            }
+          });
+        },
 
-    intervals: function(callback) {
-      larkin.queryPg("burwell", sql.intervals, [req.query.lng, req.query.lat], function(error, data) {
-        if (error) {
-          callback(error);
-        } else {
-          callback(null, data.rows);
-        }
-      });
-    },
+        intervals: function(callback) {
+          larkin.queryPg("burwell", sql.intervals.replace(':scale', scale), [req.query.lng, req.query.lat], function(error, data) {
+            if (error) {
+              callback(error);
+            } else {
+              callback(null, data.rows);
+            }
+          });
+        },
 
-    map_units: function(callback) {
-      larkin.queryPg("burwell", sql.map_units, ['SRID=4326;POINT(' + req.query.lng + ' ' + req.query.lat + ')'], function(error, data) {
+        map_units: function(callback) {
+          larkin.queryPg("burwell", sql.map_units.replace(/:scale/g, scale), [req.query.lng, req.query.lat], function(error, data) {
+            if (error) {
+              callback(error);
+            } else {
+              callback(null, data.rows);
+            }
+          })
+        },
+        //
+        // places: function(callback) {
+        //   larkin.queryPg("rockd", sql.places, ['SRID=4326;POINT(' + req.query.lng + ' ' + req.query.lat + ')'], function(error, data) {
+        //     if (error) {
+        //       callback(error);
+        //     } else {
+        //       callback(null, data.rows);
+        //     }
+        //   })
+        // }
+      }, function(error, results) {
         if (error) {
-          callback(error);
+          cb(error)
         } else {
-          callback(null, data.rows);
+          cb(null, results)
         }
       })
-    },
-    //
-    // places: function(callback) {
-    //   larkin.queryPg("rockd", sql.places, ['SRID=4326;POINT(' + req.query.lng + ' ' + req.query.lat + ')'], function(error, data) {
-    //     if (error) {
-    //       callback(error);
-    //     } else {
-    //       callback(null, data.rows);
-    //     }
-    //   })
-    // }
-  }, function(error, results) {
-    if (error) {
-      larkin.error(req, res, next, error);
-    } else {
-      larkin.sendData(req, res, next, {
-        format: (api.acceptedFormats.standard[req.query.format]) ? req.query.format : "json",
-        bare: (api.acceptedFormats.bare[req.query.format]) ? true : false,
-        compact: true
-      }, {
-        data: results
-      });
     }
-  });
-
+  ], function(error, results) {
+    if (error) {
+      return larkin.error(req, res, next, error, 500)
+    }
+    larkin.sendData(req, res, next, {
+      format: (api.acceptedFormats.standard[req.query.format]) ? req.query.format : "json",
+      bare: (api.acceptedFormats.bare[req.query.format]) ? true : false,
+      compact: true
+    }, {
+      data: results
+    });
+  })
 }
