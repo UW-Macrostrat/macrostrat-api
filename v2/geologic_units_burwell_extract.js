a292760c67a8698d7eda4d084d6a304462a17212
John J Czaplewski <john@czaplewski.org>
2017-03-31 14:58:22 -0500

    use gdal instead of node buffers

--- v2/geologic_units_burwell_extract.js
+++ v2/geologic_units_burwell_extract.js
@@ -21,27 +21,39 @@ module.exports = (req, res, next) => {
   req.query.max_lng = larkin.normalizeLng(parseFloat(req.query.max_lng).toFixed(4))
   req.query.max_lat = parseFloat(req.query.max_lat).toFixed(4)
 
-  let datasetName = Math.random().toString(36).substring(5)
+  const datasetName = Math.random().toString(36).substring(5)
   fs.mkdirSync(`${__dirname}/${datasetName}`)
 
   async.parallel({
-    elevation: (callback) => {
-      larkin.queryPg('elevation', `
-        SELECT ST_AsTIFF(ST_Union(rast)) AS rast
-        FROM sources.srtm1
-        WHERE ST_Intersects(rast, ST_SetSRID(ST_MakeBox2D(ST_MakePoint($1, $2), ST_MakePoint($3, $4)), 4326))
-      `, [req.query.min_lng, req.query.min_lat, req.query.max_lng, req.query.max_lat], (error, data) => {
-        if (error) return callback(error)
+    /*
+    You could also use gdal
+    gdal_translate -of GTiff PG:'host=localhost port=5439 dbname=elevation user=john schema=sources table=srtm1 where="ST_Intersects(rast, ST_SetSRID(ST_MakeBox2D(ST_MakePoint(-117.5, 44.5), ST_MakePoint(-115.5, 46)), 4326))" mode=2' test.tiff
+    */
 
-        let result = Buffer.concat(data.rows.map(d => { return new Buffer(d.rast.buffer) }))
-        fs.writeFile(`${__dirname}/${datasetName}/srtm1.tiff`, result, 'binary', (error) => {
-          if (error) {
-            console.log(error)
-          }
-          callback(null)
-        })
+    elevation: (callback) => {
+      exec(`gdal_translate -of GTiff PG:'host=${credentials.pg.host} port=${credentials.pg.port} dbname=elevation user=${credentials.pg.user} schema=sources table=srtm1 where="ST_Intersects(rast, ST_SetSRID(ST_MakeBox2D(ST_MakePoint(-117.5, 44.5), ST_MakePoint(-115.5, 46)), 4326))" mode=2' ${__dirname}/${datasetName}/srtm1.tiff`, (error, stderror, stdout) => {
+        if (error) return callback(error)
+        callback(null)
       })
     },
+    // elevation: (callback) => {
+    //   larkin.queryPg('elevation', `
+    //     SELECT ST_AsTIFF(ST_Union(rast)) AS rast
+    //     FROM sources.srtm1
+    //     WHERE ST_Intersects(rast, ST_SetSRID(ST_MakeBox2D(ST_MakePoint($1, $2), ST_MakePoint($3, $4)), 4326))
+    //   `, [req.query.min_lng, req.query.min_lat, req.query.max_lng, req.query.max_lat], (error, data) => {
+    //     if (error) return callback(error)
+    //     console.log('got raster')
+    //     const result = Buffer.concat(data.rows.map(d => { return new Buffer(d.rast.buffer) }))
+    //     fs.writeFile(`${__dirname}/${datasetName}/srtm1.tiff`, result, 'binary', (error) => {
+    //       if (error) {
+    //         console.log(error)
+    //       }
+    //       console.log('wrote', `${__dirname}/${datasetName}/srtm1.tiff`)
+    //       callback(null)
+    //     })
+    //   })
+    // },
     geology_shapefile: (callback) => {
       exec(`pgsql2shp -u ${credentials.pg.user} -h ${credentials.pg.host} -p ${credentials.pg.port} -f ${__dirname}/${datasetName}/geology.shp burwell "SELECT map_id, ST_Intersection(geom, ST_SetSRID(ST_MakeBox2D(ST_MakePoint(${req.query.min_lng}, ${req.query.min_lat}), ST_MakePoint(${req.query.max_lng}, ${req.query.max_lat})), 4326)) AS geom FROM carto.large WHERE ST_Intersects(geom, ST_SetSRID(ST_MakeBox2D(ST_MakePoint(${req.query.min_lng}, ${req.query.min_lat}), ST_MakePoint(${req.query.max_lng}, ${req.query.max_lat})), 4326))"`, (error) => {
         if (error) {
@@ -59,10 +71,10 @@ module.exports = (req, res, next) => {
         if (error) {
           console.log(error)
         }
-        let csv = json2csv({
+
+        fs.writeFile(`${__dirname}/${datasetName}/attributes.csv`, json2csv({
           data: result.rows
-        })
-        fs.writeFile(`${__dirname}/${datasetName}/attributes.csv`, csv, (error, result) => {
+        }), (error, result) => {
           if (error) {
             console.log(error)
           }
@@ -76,12 +88,16 @@ module.exports = (req, res, next) => {
       return larkin.error(req, res, next, 'Something went wrong', 500)
     }
     // Zip the resultant files
-    exec(`zip -rj ${__dirname}/${datasetName}.zip ${__dirname}/${datasetName}/*`, (error, stderror, stdout) => {
+    exec(`zip -rj ${__dirname}/${datasetName}.zip ${__dirname}/${datasetName}`, (error, stderror, stdout) => {
       // Send the zip archive to the client
-      res.sendFile(`${__dirname}/${datasetName}.zip`)
-      // Delete the original files
-      rimraf(`${__dirname}/${datasetName}`, () => {
-        fs.unlinkSync(`${__dirname}/${datasetName}.zip`)
+      res.sendFile(`${__dirname}/${datasetName}.zip`, (error) => {
+        if (error) {
+          console.log(error)
+        }
+        // Delete the original files
+        rimraf(`${__dirname}/${datasetName}`, () => {
+          fs.unlinkSync(`${__dirname}/${datasetName}.zip`)
+        })
       })
     })
   })
