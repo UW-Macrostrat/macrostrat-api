f82daec84ae6a3cdac186c9cac4845c7a3eead8d
John J Czaplewski <john@czaplewski.org>
2018-01-05 11:13:13 -0600

    Remove reliance on mapzen elevation api

--- v2/elevation.js
+++ v2/elevation.js
@@ -1,9 +1,6 @@
 'use strict'
 const api = require('./api')
 const larkin = require('./larkin')
-const https = require('https')
-const polyline = require('@mapbox/polyline')
-const credentials = require('./credentials')
 
 module.exports = (req, res, next, cb) => {
   if (Object.keys(req.query).length < 1) {
@@ -12,30 +9,34 @@ module.exports = (req, res, next, cb) => {
   if ((req.query.lat && req.query.lng) || 'sample' in req.query) {
     let lat = req.query.lat || 43.07
     let lng = larkin.normalizeLng(req.query.lng) || -89.4
+    let point = `POINT(${lng} ${lat})`
 
-    https.get(`https://elevation.mapzen.com/height?json={"range":false,"shape":[{"lat":${lat},"lon":${lng}}]}&api_key=${credentials.mapzen_key}`, (response) => {
-      let body = ''
-
-      response.on('data', (chunk) => {
-        body += chunk
-      })
-      response.on('end', () => {
-        let mapzenResponse = body
-        try {
-          mapzenResponse = JSON.parse(mapzenResponse)
-        } catch(e) {
-          if (cb) return cb(e)
-          return larkin.error(req, res, next, 'Error retrieving data from Mapzen', 500)
-        }
-        let toSend = [{ elevation: mapzenResponse.height[0] || null }]
-
-        if (cb) return cb(null, toSend)
-        larkin.sendData(req, res, next, {
-          format: (api.acceptedFormats.standard[req.query.format]) ? req.query.format : 'json',
-          compact: true
-        }, {
-          data: toSend
-        })
+    larkin.queryPg('elevation', `
+      WITH first AS (
+          SELECT ST_Value(rast, 1, ST_GeomFromText($1, 4326)) AS elevation, 1 as priority
+          FROM sources.srtm1
+          WHERE ST_Intersects(ST_GeomFromText($1, 4326), rast)
+          UNION ALL
+          SELECT ST_Value(rast, 1, ST_GeomFromText($1, 4326)) AS elevation, 2 as priority
+          FROM sources.etopo1
+          WHERE ST_Intersects(ST_GeomFromText($1, 4326), rast)
+      )
+      SELECT elevation
+      FROM first
+      WHERE elevation IS NOT NULL
+      ORDER BY priority ASC
+      LIMIT 1
+    `, [ point ], (error, result) => {
+      if (error) {
+        if (cb) return cb(error)
+        return larkin.error(req, res, next, 'Error fetching elevation data')
+      }
+      if (cb) return cb(null, result.rows)
+      larkin.sendData(req, res, next, {
+        format: (api.acceptedFormats.standard[req.query.format]) ? req.query.format : 'json',
+        compact: true
+      }, {
+        data: result.rows
       })
     })
 
@@ -64,40 +65,35 @@ module.exports = (req, res, next, cb) => {
       SELECT
         ST_X(geom) AS lng,
         ST_Y(geom) AS lat,
-        round((ST_Distance_Sphere(geom, $2) * 0.001)::numeric, 2)::float AS d
+        round((ST_Distance_Sphere(geom, $2) * 0.001)::numeric, 2)::float AS d,
+        (
+          SELECT elevation
+          FROM (
+              SELECT ST_Value(rast, 1, geom) AS elevation, 1 as priority
+              FROM sources.srtm1
+              WHERE ST_Intersects(geom, rast)
+              UNION ALL
+              SELECT ST_Value(rast, 1, geom) AS elevation, 2 as priority
+              FROM sources.etopo1
+              WHERE ST_Intersects(geom, rast)
+          ) first
+          WHERE elevation IS NOT NULL AND elevation != 0
+          ORDER BY priority ASC
+          LIMIT 1
+        ) AS elevation
       FROM first
     `, [linestring, westPoint], (error, result) => {
       if (error) {
         if (cb) return cb(error)
         return larkin.error(req, res, next, 'Internal error', 500)
       }
-      let pts = result.rows
-
-      let encodedPolyline = polyline.encode(pts.map(pt => { return [pt.lat, pt.lng] }), 6)
-
-      https.get(`https://elevation.mapzen.com/height?json={"range":true,"encoded_polyline":"${encodedPolyline}"}&api_key=${credentials.mapzen_key}`, (response) => {
-        let body = ''
-
-        response.on('data', (chunk) => {
-          body += chunk
-        })
-
-        response.on('end', () => {
-          let elevationResult = JSON.parse(body)
-
-          if (cb) return cb(null, result.rows)
-          larkin.sendData(req, res, next, {
-            format: (api.acceptedFormats.standard[req.query.format]) ? req.query.format : 'json',
-            compact: true
-          }, {
-            data: pts.map((pt, i) => {
-              pt.elevation = elevationResult.range_height[i][1]
-              return pt
-            })
-          })
-        })
+      if (cb) return cb(null, result.rows)
+      larkin.sendData(req, res, next, {
+        format: (api.acceptedFormats.standard[req.query.format]) ? req.query.format : 'json',
+        compact: true
+      }, {
+        data: result.rows
       })
-
     })
   } else {
     return larkin.error(req, res, next, 'Invalid Parameters', 401)
