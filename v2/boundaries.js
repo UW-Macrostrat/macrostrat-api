e81637751974fbbad250a6a1780c2c887c7da0e8
John J Czaplewski <john@czaplewski.org>
2018-04-24 13:24:57 -0500

    Add super simple boundaries route

---
+++ v2/boundaries.js
@@ -0,0 +1,41 @@
+const larkin = require('./larkin')
+const dbgeo = require('dbgeo')
+
+module.exports = (req, res, next) => {
+  if (Object.keys(req.query).length < 1) {
+    return larkin.info(req, res, next);
+  }
+
+  if (!req.query.boundary_id) {
+    return larkin.error(req, res, next)
+  }
+  req.query.boundary_id = req.query.boundary_id.split(',')
+
+  larkin.queryPg('burwell', `
+    SELECT boundary_id, ST_Simplify(geom, 0.1) AS geom
+    FROM geologic_boundaries.boundaries
+    WHERE boundary_id = ANY($1)
+  `, [ req.query.boundary_id ], (error, result) => {
+    if (error || !result) {
+      return larkin.error(req, res, next)
+    }
+
+    dbgeo.parse(result.rows, {
+      outputFormat: 'geojson',
+      precision: 4
+    }, (error, result) => {
+      if (error) {
+        return larkin.error(req, res, next)
+      }
+
+      larkin.sendData(req, res, next, {
+        format: 'json',
+        bare: true,
+        compact: true
+      }, {
+        data: result
+      })
+    })
+  })
+
+}
