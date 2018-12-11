6570770566e3fed16e4fe29d4cde58a4663e436b
John J Czaplewski <john@czaplewski.org>
2018-12-11 14:42:24 -0600

    Add greedy lith filtering

--- v2/mobile/map_filter.js
+++ v2/mobile/map_filter.js
@@ -1,19 +1,10 @@
 'use strict'
-
 const larkin = require('../larkin')
 
 module.exports = (req, res, next) => {
-  const validFilters = [
-    'lith_class',
-    'lith_type',
-    'lith',
-    'lith_id',
-    'strat_name_id',
-    'concept_id'
-  ]
-
   let where = []
   let params = []
+  // Normal (strict) liths
   if (req.query.lith_class) {
     where.push(`lith_classes && $${where.length + 1}`)
     params.push(req.query.lith_class.split(','))
@@ -26,6 +17,20 @@ module.exports = (req, res, next) => {
     where.push(`lith_ids && $${where.length + 1}`)
     params.push(req.query.lith_id.split(','))
   }
+  // All liths
+  if (req.query.all_lith_class) {
+    where.push(`all_lith_classes && $${where.length + 1}`)
+    params.push(req.query.all_lith_class.split(','))
+  }
+  if (req.query.all_lith_type) {
+    where.push(`all_lith_types && $${where.length + 1}`)
+    params.push(req.query.all_lith_type.split(','))
+  }
+  if (req.query.all_lith_id) {
+    where.push(`all_lith_ids && $${where.length + 1}`)
+    params.push(req.query.all_lith_id.split(','))
+  }
+  // Strat names
   if (req.query.strat_name_id) {
     where.push(`strat_name_children && $${where.length + 1}`)
     params.push(req.query.strat_name_id.split(','))
