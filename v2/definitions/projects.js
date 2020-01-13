bcca8b3305e0bb38294395fe57dce49a4c14e03a
Shanan Peters <peters@geology.wisc.edu>
2020-01-13 11:30:33 -0600

    updated measurements and projects

--- v2/definitions/projects.js
+++ v2/definitions/projects.js
@@ -5,7 +5,7 @@ module.exports = function(req, res, next, cb) {
   if (Object.keys(req.query).length < 1) {
     return larkin.info(req, res, next);
   }
-  var sql = "SELECT projects.id AS project_id, project, timescale_id, COUNT(DISTINCT units_sections.unit_id) AS t_units FROM projects LEFT JOIN cols ON projects.id = cols.project_id LEFT JOIN units_sections ON units_sections.col_id = cols.id GROUP BY projects.id";
+  var sql = "SELECT projects.id AS project_id, project, descrip, timescale_id, COUNT(DISTINCT units_sections.col_id) AS t_cols, COUNT(DISTINCT units_sections.unit_id) AS t_units FROM projects LEFT JOIN cols ON projects.id = cols.project_id LEFT JOIN units_sections ON units_sections.col_id = cols.id GROUP BY projects.id";
 
   if ("sample" in req.query) {
     sql += " LIMIT 5";
