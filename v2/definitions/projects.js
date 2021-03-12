9be24a3247c66de4bb3130009e53d9c536e606df
Shanan Peters <peters@geology.wisc.edu>
2021-03-12 16:30:36 -0600

    updated project options, better documentation

--- v2/definitions/projects.js
+++ v2/definitions/projects.js
@@ -5,13 +5,23 @@ module.exports = function(req, res, next, cb) {
   if (Object.keys(req.query).length < 1) {
     return larkin.info(req, res, next);
   }
-  var sql = "SELECT projects.id AS project_id, project, descrip, timescale_id, COUNT(DISTINCT units_sections.col_id) AS t_cols, COUNT(DISTINCT units_sections.unit_id) AS t_units FROM projects LEFT JOIN cols ON projects.id = cols.project_id LEFT JOIN units_sections ON units_sections.col_id = cols.id GROUP BY projects.id";
+  var sql = "SELECT projects.id AS project_id, project, descrip, timescale_id, COUNT(DISTINCT units_sections.col_id) AS t_cols, COUNT(DISTINCT units_sections.unit_id) AS t_units FROM projects LEFT JOIN cols ON projects.id = cols.project_id LEFT JOIN units_sections ON units_sections.col_id = cols.id";
 
-  if ("sample" in req.query) {
-    sql += " LIMIT 5";
+  var where = []
+  var params = {}
+
+  if (req.query.project_id) {
+    where.push("projects.id in (:project_id)")
+    params["project_id"] = larkin.parseMultipleIds(req.query.project_id)
   }
+  if (where.length) {
+    sql += ` WHERE ${where.join(' AND ')}`
+  }
+  sql += " GROUP BY projects.id";
 
-  larkin.query(sql, [], function(error, data) {
+  console.log(sql)
+
+  larkin.query(sql, params, function(error, data) {
     if (error) {
       if (cb) {
         return cb(error);
