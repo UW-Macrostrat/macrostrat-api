aff3184dd013b808a69457507bfb062d6be2189f
Daven Quinn <dev@davenquinn.com>
2023-03-11 01:21:29 -0600

    Start eventaul transition to yarn

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-npm install --prefix ./v1 ./v1
-npm install --prefix ./v2 ./v2
+yarn install --prefix ./v1 ./v1
+yarn install --prefix ./v2 ./v2
 #cp v1/credentials.example.js v1/credentials.js
 #cp v2/credentials.example.js v2/credentials.js
