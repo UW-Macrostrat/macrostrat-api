f84c7484818d1b10409ee07500ce413f6189141c
Daven Quinn <code@davenquinn.com>
2024-01-18 02:51:34 -0600

    Works with a local MariaDB instance

--- v2/index.ts
+++ v2/index.ts
@@ -1,6 +1,9 @@
 var api = require("./api"),
   larkin = require("./larkin");
 
+// Establish a connection to the database
+larkin.connectMySQL();
+
 // Set up the column and unit cache
 larkin.setupCache();
 
