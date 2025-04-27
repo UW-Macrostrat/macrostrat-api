461f6173ac0914f89ff09d75cf3b9d6a44bb7cc0
Daven Quinn <code@davenquinn.com>
2025-04-27 06:03:12 -0500

    Added info to /meta route

--- v2/meta.ts
+++ v2/meta.ts
@@ -16,22 +16,22 @@ module.exports = function (req, res, next) {
           "All data are licensed under Creative Commons BY 4.0 International",
         citation:
           "Peters, S.E. et al. Macrostrat: a platform for geological data integration and deep-time research. https://macrostrat.org, accessed on (date)",
-        api: ["John J Czaplewski", "Puneet Kishor", "Shanan E Peters"],
+        api: ["John J Czaplewski", "Daven Quinn", "Amy Fromandi", "Puneet Kishor", "Shanan E Peters"],
+        version: require("../package.json").version,
+        support: {
+          current_funding:
+            "U.S. National Science Foundation Grant EAR-1150082 and ICER-1440312. Additional support from the University of Wisconsin-Madison Dept. of Geoscience.",
+          previous_funding:
+            "Initial database development supported by the United States Geological Survey and the American Chemical Society.",
+        },
+        acknowledgements: {
+          data_entry:
+            "University of Michigan Undergraduate Research Opportunity program, Noel Heim, Deb Rook, Neal Auchter, Annaka Clement, Clay Kelly.",
+          contributors: ["Jon Husson"],
+          data_providers:
+            "The data synthesized in Macrostrat reflect the combined work of hundreds of field-based geoscientists. These contributors are too many to cite individually, but see /defs/refs?all and /defs/sources?all for a list of primary sources supplying data and the references therein.",
+        },
       },
-      support: {
-        current_funding:
-          "U.S. National Science Foundation Grant EAR-1150082 and ICER-1440312. Additional support from the University of Wisconsin-Madison Dept. of Geoscience.",
-        previous_funding:
-          "Initial database development supported by the United States Geological Survey and the American Chemical Society.",
-      },
-      acknowledgements: {
-        data_entry:
-          "University of Michigan Undergraduate Research Opportunity program, Noel Heim, Deb Rook, Neal Auchter, Annaka Clement, Clay Kelly.",
-        contributors: ["Jon Husson"],
-        data_providers:
-          "The data synthesized in Macrostrat reflect the combined work of hundreds of field-based geoscientists. These contributors are too many to cite individually, but see /defs/refs?all and /defs/sources?all for a list of primary sources supplying data and the references therein.",
-      },
-      version: require("../package.json").version,
     },
   );
 };
