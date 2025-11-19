93b215800dc043ac77678a1156bf30847294a1d2
Daven Quinn <code@davenquinn.com>
2025-11-19 15:30:12 -0600

    API tests pass except for small differences with production

--- api-tests/settings.ts
+++ api-tests/settings.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  host_prod: "https://www.macrostrat.org/api/v2",
+  host_prod: "https://v2.macrostrat.org/api/v2",
   host_local: "http://localhost:5002/v2",
   host: "http://localhost:5002",
 };
