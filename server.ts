b530e2bd0ea33d1e426c1de4b66c6f153a85162a
Daven Quinn <code@davenquinn.com>
2025-11-19 18:36:24 -0600

    Completely remove Universal Analytics

--- server.ts
+++ server.ts
@@ -9,37 +9,6 @@ var express = require("express"),
   defs = require("./v2/defs"),
   app = express();
 
-var ua = require("universal-analytics");
-
-ua(
-  "UA-75785431-1",
-  null,
-  {},
-  {
-    dh: "https://macrostrat.org/api",
-  },
-);
-
-app.use(function (req, res, next) {
-  // Ignore requests from the status server
-  if (req.query && req.query.skip) {
-    return next();
-  }
-
-  var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-  var visitor = ua("UA-75785431-1", { https: true });
-
-  visitor
-    .pageview({
-      dp: req.originalUrl,
-      uip: ip || "0.0.0.0",
-      an: req.query && req.query.referrer ? req.query.referrer : "",
-      av: req.query && req.query.version ? req.query.version : "",
-    })
-    .send();
-  next();
-});
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
